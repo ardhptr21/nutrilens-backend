8e35aa6acbd99ea4d6f63035c354a9f5d158815c
zeinmhasan <hasro71@gmail.com>
2025-12-02 23:12:29 +0700

    nyesuaikan dikit untuk integrasi fe

--- src/api/nutrition/nutritionRepository.ts
+++ src/api/nutrition/nutritionRepository.ts
@@ -1,78 +1,82 @@
 import { prisma } from "@/common/lib/prisma";
 
 class NutritionRepository {
-	async findNutritionByUserIdAndDate(userId: string, date: Date) {
-		return await prisma.nutrition.findFirst({
-			where: {
-				userId,
-				logAt: date,
-			},
-		});
-	}
+  async findNutritionByUserIdAndDate(userId: string, date: Date) {
+    return await prisma.nutrition.findFirst({
+      where: {
+        userId,
+        logAt: date,
+      },
+    });
+  }
 
-	async findNutritionWithMealsByUserIdAndDate(userId: string, date: Date) {
-		return await prisma.nutrition.findFirst({
-			where: {
-				userId,
-				logAt: {
-					gte: date,
-					lte: new Date(date.getTime() + 24 * 60 * 60 * 1000),
-				},
-			},
-			include: {
-				meals: true,
-				user: {
-					select: {
-						preference: true,
-					},
-				},
-			},
-		});
-	}
+  async findNutritionWithMealsByUserIdAndDate(
+    userId: string,
+    startDate: Date,
+    endDate: Date
+  ) {
+    return await prisma.nutrition.findFirst({
+      where: {
+        userId,
+        logAt: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+      include: {
+        meals: true,
+        user: {
+          select: {
+            preference: true,
+          },
+        },
+      },
+    });
+  }
 
-	async createNutrition(
-		userId: string,
-		logAt: Date,
-		targetCal: number,
-		targetFat: number,
-		targetCarbs: number,
-		targetProtein: number,
-	) {
-		return await prisma.nutrition.create({
-			data: {
-				userId,
-				logAt,
-				targetCal: targetCal || 2500,
-				targetFat: targetFat || 70,
-				targetCarbs: targetCarbs || 275,
-				targetProtein: targetProtein || 50,
-			},
-		});
-	}
+  async createNutrition(
+    userId: string,
+    logAt: Date,
+    targetCal: number,
+    targetFat: number,
+    targetCarbs: number,
+    targetProtein: number
+  ) {
+    return await prisma.nutrition.create({
+      data: {
+        userId,
+        logAt,
+        targetCal: targetCal || 2500,
+        targetFat: targetFat || 70,
+        targetCarbs: targetCarbs || 275,
+        targetProtein: targetProtein || 50,
+      },
+    });
+  }
 
-	async createMeal(
-		nutritionId: string,
-		name: string,
-		image: string | null,
-		description: string | null,
-		cal: number,
-		fat: number,
-		protein: number,
-		carbs: number,
-	) {
-		return await prisma.meal.create({
-			data: {
-				nutritionId,
-				name,
-				image,
-				description,
-				cal,
-				fat,
-				protein,
-				carbs,
-			},
-		});
-	}
+  async createMeal(
+    nutritionId: string,
+    name: string,
+    image: string | null,
+    description: string | null,
+    cal: number,
+    fat: number,
+    protein: number,
+    carbs: number
+  ) {
+    return await prisma.meal.create({
+      data: {
+        nutritionId,
+        name,
+        image,
+        description,
+        cal,
+        fat,
+        protein,
+        carbs,
+      },
+    });
+  }
 }
 
 export const nutritionRepository = new NutritionRepository();
--- src/api/nutrition/nutritionService.ts
+++ src/api/nutrition/nutritionService.ts
@@ -8,180 +8,240 @@ import type { MealUploadModel, ScanModel } from "./nutritionModel";
 import { nutritionRepository } from "./nutritionRepository";
 
 class NutritionService {
-	public async scan(data: ScanModel["body"]) {
-		const uploaded = await gemini.files.upload({
-			file: data.image,
-		});
-		if (!uploaded.uri || !uploaded.mimeType)
-			return ServiceResponse.failure("File upload failed", null, StatusCodes.INTERNAL_SERVER_ERROR);
+  public async scan(data: ScanModel["body"]) {
+    const uploaded = await gemini.files.upload({
+      file: data.image,
+    });
+    if (!uploaded.uri || !uploaded.mimeType)
+      return ServiceResponse.failure(
+        "File upload failed",
+        null,
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
 
-		const prompts = [
-			"Provide a detailed nutritional analysis of the food item in the image, including calorie, fat, protein, and carbs in grams. Format the response as a 'only' JSON object with appropriate keys. The json should be:\n",
-			`{"name:"(in bahasa)","cal":number,"fat":number,"protein":number,"carbs":number}`,
-		];
+    const prompts = [
+      "Provide a detailed nutritional analysis of the food item in the image, including calorie, fat, protein, and carbs in grams. Format the response as a 'only' JSON object with appropriate keys. The json should be:\n",
+      `{"name:"(in bahasa)","cal":number,"fat":number,"protein":number,"carbs":number}`,
+    ];
 
-		if (data.detail) prompts.push(`\nAdditional details: ${data.detail}\n`);
+    if (data.detail) prompts.push(`\nAdditional details: ${data.detail}\n`);
 
-		console.log(prompts.join(""));
+    console.log(prompts.join(""));
 
-		const result = await gemini.models.generateContent({
-			model: "gemini-2.0-flash",
-			contents: createUserContent([createPartFromUri(uploaded.uri, uploaded.mimeType), "\n\n", ...prompts]),
-		});
+    const result = await gemini.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: createUserContent([
+        createPartFromUri(uploaded.uri, uploaded.mimeType),
+        "\n\n",
+        ...prompts,
+      ]),
+    });
 
-		if (!result.text)
-			return ServiceResponse.failure("Failed to generate nutrition data", null, StatusCodes.INTERNAL_SERVER_ERROR);
+    if (!result.text)
+      return ServiceResponse.failure(
+        "Failed to generate nutrition data",
+        null,
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
 
-		const jsonStart = result.text.indexOf("{");
-		const jsonEnd = result.text.lastIndexOf("}") + 1;
-		const jsonString = result.text.substring(jsonStart, jsonEnd);
-		const nutritionData = JSON.parse(jsonString);
+    const jsonStart = result.text.indexOf("{");
+    const jsonEnd = result.text.lastIndexOf("}") + 1;
+    const jsonString = result.text.substring(jsonStart, jsonEnd);
+    const nutritionData = JSON.parse(jsonString);
 
-		return ServiceResponse.success("Nutrition data retrieved", nutritionData, StatusCodes.OK);
-	}
+    return ServiceResponse.success(
+      "Nutrition data retrieved",
+      nutritionData,
+      StatusCodes.OK
+    );
+  }
 
-	public async getStatistics(userId: string, date: Date) {
-		try {
-			const startOfDay = new Date(date);
-			startOfDay.setHours(0, 0, 0, 0);
+  public async getStatistics(userId: string, date: Date) {
+    try {
+      const startOfDay = new Date(date);
+      // Ensure we're working with UTC midnight
+      startOfDay.setUTCHours(0, 0, 0, 0);
 
-			const nutrition = await nutritionRepository.findNutritionWithMealsByUserIdAndDate(userId, startOfDay);
+      const endOfDay = new Date(startOfDay);
+      endOfDay.setUTCHours(23, 59, 59, 999);
 
-			if (!nutrition) {
-				if (startOfDay.getTime() === new Date().setHours(0, 0, 0, 0)) {
-					const userPreference = await preferenceRepository.findByUserId(userId);
-					const newNutrition = await nutritionRepository.createNutrition(
-						userId,
-						startOfDay,
-						Number(userPreference?.targetCal),
-						Number(userPreference?.targetFat),
-						Number(userPreference?.targetCarbs),
-						Number(userPreference?.targetProtein),
-					);
-					return ServiceResponse.success(
-						"Nutrition statistics retrieved",
-						{
-							id: newNutrition.id,
-							logAt: newNutrition.logAt,
-							cal: 0,
-							fat: 0,
-							protein: 0,
-							carbs: 0,
-							targetCal: Number(newNutrition.targetCal),
-							targetFat: Number(newNutrition.targetFat),
-							targetCarbs: Number(newNutrition.targetCarbs),
-							targetProtein: Number(newNutrition.targetProtein),
-							meals: [],
-						},
-						StatusCodes.OK,
-					);
-				}
-				return ServiceResponse.success("No nutrition data found for this date", null, StatusCodes.OK);
-			}
+      const nutrition =
+        await nutritionRepository.findNutritionWithMealsByUserIdAndDate(
+          userId,
+          startOfDay,
+          endOfDay
+        );
 
-			let totalCal = 0;
-			let totalFat = 0;
-			let totalProtein = 0;
-			let totalCarbs = 0;
+      if (!nutrition) {
+        const today = new Date();
+        today.setUTCHours(0, 0, 0, 0);
 
-			for (const meal of nutrition.meals) {
-				totalCal += Number(meal.cal);
-				totalFat += Number(meal.fat);
-				totalProtein += Number(meal.protein);
-				totalCarbs += Number(meal.carbs);
-			}
+        if (startOfDay.getTime() === today.getTime()) {
+          const userPreference = await preferenceRepository.findByUserId(
+            userId
+          );
+          const newNutrition = await nutritionRepository.createNutrition(
+            userId,
+            startOfDay,
+            Number(userPreference?.targetCal),
+            Number(userPreference?.targetFat),
+            Number(userPreference?.targetCarbs),
+            Number(userPreference?.targetProtein)
+          );
+          return ServiceResponse.success(
+            "Nutrition statistics retrieved",
+            {
+              id: newNutrition.id,
+              logAt: newNutrition.logAt,
+              cal: 0,
+              fat: 0,
+              protein: 0,
+              carbs: 0,
+              targetCal: Number(newNutrition.targetCal),
+              targetFat: Number(newNutrition.targetFat),
+              targetCarbs: Number(newNutrition.targetCarbs),
+              targetProtein: Number(newNutrition.targetProtein),
+              meals: [],
+            },
+            StatusCodes.OK
+          );
+        }
+        return ServiceResponse.success(
+          "No nutrition data found for this date",
+          null,
+          StatusCodes.OK
+        );
+      }
 
-			const response = {
-				id: nutrition.id,
-				logAt: nutrition.logAt,
-				cal: totalCal,
-				fat: totalFat,
-				protein: totalProtein,
-				carbs: totalCarbs,
-				targetCal: Number(nutrition.targetCal),
-				targetFat: Number(nutrition.targetFat),
-				targetCarbs: Number(nutrition.targetCarbs),
-				targetProtein: Number(nutrition.targetProtein),
-				meals: nutrition.meals.map((meal) => ({
-					id: meal.id,
-					name: meal.name,
-					image: meal.image,
-					description: meal.description,
-					cal: Number(meal.cal),
-					fat: Number(meal.fat),
-					protein: Number(meal.protein),
-					carbs: Number(meal.carbs),
-					createdAt: meal.createdAt,
-				})),
-			};
+      let totalCal = 0;
+      let totalFat = 0;
+      let totalProtein = 0;
+      let totalCarbs = 0;
 
-			return ServiceResponse.success("Nutrition statistics retrieved", response, StatusCodes.OK);
-		} catch (error) {
-			console.error("Error fetching nutrition statistics:", error);
-			return ServiceResponse.failure("Failed to fetch nutrition statistics", null, StatusCodes.INTERNAL_SERVER_ERROR);
-		}
-	}
+      for (const meal of nutrition.meals) {
+        totalCal += Number(meal.cal);
+        totalFat += Number(meal.fat);
+        totalProtein += Number(meal.protein);
+        totalCarbs += Number(meal.carbs);
+      }
 
-	public async getStatisticsToday(userId: string) {
-		const today = new Date();
-		today.setHours(0, 0, 0, 0);
-		return this.getStatistics(userId, today);
-	}
+      const response = {
+        id: nutrition.id,
+        logAt: nutrition.logAt,
+        cal: totalCal,
+        fat: totalFat,
+        protein: totalProtein,
+        carbs: totalCarbs,
+        targetCal: Number(nutrition.targetCal),
+        targetFat: Number(nutrition.targetFat),
+        targetCarbs: Number(nutrition.targetCarbs),
+        targetProtein: Number(nutrition.targetProtein),
+        meals: nutrition.meals.map((meal) => ({
+          id: meal.id,
+          name: meal.name,
+          image: meal.image,
+          description: meal.description,
+          cal: Number(meal.cal),
+          fat: Number(meal.fat),
+          protein: Number(meal.protein),
+          carbs: Number(meal.carbs),
+          createdAt: meal.createdAt,
+        })),
+      };
 
-	public async uploadMeal(data: MealUploadModel["body"], userId: string) {
-		try {
-			const userPreference = await preferenceRepository.findByUserId(userId);
+      return ServiceResponse.success(
+        "Nutrition statistics retrieved",
+        response,
+        StatusCodes.OK
+      );
+    } catch (error) {
+      console.error("Error fetching nutrition statistics:", error);
+      return ServiceResponse.failure(
+        "Failed to fetch nutrition statistics",
+        null,
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
 
-			const today = new Date();
-			today.setHours(0, 0, 0, 0);
+  public async getStatisticsToday(userId: string) {
+    const today = new Date();
+    today.setUTCHours(0, 0, 0, 0);
+    return this.getStatistics(userId, today);
+  }
 
-			let nutrition = await nutritionRepository.findNutritionByUserIdAndDate(userId, today);
+  public async uploadMeal(data: MealUploadModel["body"], userId: string) {
+    try {
+      const userPreference = await preferenceRepository.findByUserId(userId);
 
-			if (!nutrition) {
-				nutrition = await nutritionRepository.createNutrition(
-					userId,
-					today,
-					Number(userPreference?.targetCal),
-					Number(userPreference?.targetFat),
-					Number(userPreference?.targetCarbs),
-					Number(userPreference?.targetProtein),
-				);
-			}
+      if (!userPreference) {
+        return ServiceResponse.failure(
+          "User preference not found. Please setup your nutrition preferences first",
+          null,
+          StatusCodes.BAD_REQUEST
+        );
+      }
 
-			const imagePath = await FileManager.upload(`meals/${userId}`, data.image);
+      const today = new Date();
+      today.setUTCHours(0, 0, 0, 0);
 
-			const meal = await nutritionRepository.createMeal(
-				nutrition.id,
-				data.name,
-				imagePath,
-				data.description || null,
-				data.cal,
-				data.fat,
-				data.protein,
-				data.carbs,
-			);
+      let nutrition = await nutritionRepository.findNutritionByUserIdAndDate(
+        userId,
+        today
+      );
 
-			return ServiceResponse.success(
-				"Meal with image created successfully",
-				{
-					id: meal.id,
-					nutritionId: nutrition.id,
-					name: meal.name,
-					image: meal.image,
-					description: meal.description,
-					cal: Number(meal.cal),
-					fat: Number(meal.fat),
-					protein: Number(meal.protein),
-					carbs: Number(meal.carbs),
-					createdAt: meal.createdAt,
-				},
-				StatusCodes.CREATED,
-			);
-		} catch (error) {
-			console.error("Error creating meal with image:", error);
-			return ServiceResponse.failure("Failed to create meal with image", null, StatusCodes.INTERNAL_SERVER_ERROR);
-		}
-	}
+      if (!nutrition) {
+        nutrition = await nutritionRepository.createNutrition(
+          userId,
+          today,
+          Number(userPreference.targetCal),
+          Number(userPreference.targetFat),
+          Number(userPreference.targetCarbs),
+          Number(userPreference.targetProtein)
+        );
+      }
+
+      const imagePath = await FileManager.upload(`meals/${userId}`, data.image);
+
+      const meal = await nutritionRepository.createMeal(
+        nutrition.id,
+        data.name,
+        imagePath,
+        data.description || null,
+        data.cal,
+        data.fat,
+        data.protein,
+        data.carbs
+      );
+
+      return ServiceResponse.success(
+        "Meal with image created successfully",
+        {
+          id: meal.id,
+          nutritionId: nutrition.id,
+          name: meal.name,
+          image: meal.image,
+          description: meal.description,
+          cal: Number(meal.cal),
+          fat: Number(meal.fat),
+          protein: Number(meal.protein),
+          carbs: Number(meal.carbs),
+          createdAt: meal.createdAt,
+        },
+        StatusCodes.CREATED
+      );
+    } catch (error) {
+      console.error("Error creating meal with image:", error);
+      const errorMessage =
+        error instanceof Error ? error.message : String(error);
+      console.error("Error details:", errorMessage);
+      return ServiceResponse.failure(
+        "Failed to create meal with image",
+        errorMessage,
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
 }
 
 export const nutritionService = new NutritionService();
